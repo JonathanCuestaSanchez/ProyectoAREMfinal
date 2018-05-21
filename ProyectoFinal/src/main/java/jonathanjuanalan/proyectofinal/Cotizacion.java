36b43491e4c74d71e7bd4e2eaade233574299ee0
unknown <2118270@labinfo.is.escuelaing.edu.co>
2018-05-21 16:14:42 -0500

    creacion json

--- ProyectoFinal/src/main/java/jonathanjuanalan/proyectofinal/Cotizacion.java
+++ ProyectoFinal/src/main/java/jonathanjuanalan/proyectofinal/Cotizacion.java
@@ -5,7 +5,7 @@
  */
 package jonathanjuanalan.proyectofinal;
 
-import org.json.JSONObject;
+import javax.json.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,19 +19,48 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @RestController
 @RequestMapping(value = "/cotizaciones")
-public class Cotizacion {
-    int contn = 3000000;
+public class Cotizacion {  
+    int temp=0;
+    int tmp2=0;
+    int tmp3=120;
     String cotizacion = "La cotizacion es de: ";
     
     @RequestMapping(method = RequestMethod.GET, value = "/cotizacion")
     public ResponseEntity<?> cotizacion(@RequestParam("cliente") String cliente, @RequestParam("estaciones") int estaciones, @RequestParam("sistema") String sistema,
-    @RequestParam("personal") String personal,@RequestParam("ciudad") String ciudad, @RequestParam("inventario") String inventario,
-    @RequestParam("software") String software, @RequestParam("transacciones") String transacciones ){
-         JsonObject cotizacion = Json.createObjectBuilder()
+    @RequestParam("personal") int personal,@RequestParam("ciudad") String ciudad, @RequestParam("inventario") String inventario,
+    @RequestParam("software") String software, @RequestParam("transacciones") int transacciones ){
+        if (sistema.equals("Windows")){
+            temp=200000;            
+        }else if (sistema.equals("Linux")){
+            temp=100000;
+        }else if (sistema.equals("Mac")){
+            temp=300000;
+        }else {
+            temp=350000;
+        }
+        if (!ciudad.equals("Bogota")){
+            tmp2=3000000;
+        }
+        if (transacciones>500){
+            tmp3=140;
+        }else{
+            tmp3=200;
+        }
+            JsonObject cotizacion = (JsonObject) Json.createObjectBuilder()
 		.add("Nombre del cotizante", cliente)
-		.add("Estaciones", Json.createObjectBuilder().add("Costo",1000000)
+                .add("Valor por sistema operativo", temp)
+		.add("Estaciones", Json.createObjectBuilder().add("Costo fijo",1000000)
                         .add("N estaciones", estaciones)
-                        .add("Total", estaciones*1000000));        
+                        .add("Costo con sistema operativo",(1000000+temp))
+                        .add("Total", (1000000+temp)* estaciones))
+                 .add("Valor de capacitacion", Json.createObjectBuilder()
+                         .add("Valor de capacitacion por persona", 150000)
+                         .add("numero de personas", personal)
+                         .add("Total", personal*150000))
+                 .add("Costos adicionales", Json.createObjectBuilder()
+                         .add("Fuera de bogota",tmp2)
+                         .add("Valor por transaccion", tmp2))
+                 .add("Total de implementacion",((1000000+temp)* estaciones)+ (personal*150000));        
         
         System.out.println("Entro a la clase cotizacion "+ estaciones + transacciones);
         return new ResponseEntity<>(cotizacion,HttpStatus.OK);
